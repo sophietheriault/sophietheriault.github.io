bf9c34c8a4936b25ea94e8980e6a992f6c2e8a69
Charles <laganierecharles@gmail.com>
2023-06-04 17:36:48 -0400

    adding measurement on item

--- menu.js
+++ menu.js
@@ -2,7 +2,17 @@ products = [
     {name:"Astronaut",link:"https://modelviewer.dev/shared-assets/models/NeilArmstrong.glb"},
     {name:"Shoe", link:"https://modelviewer.dev/shared-assets/models/glTF-Sample-Models/2.0/MaterialsVariantsShoe/glTF-Binary/MaterialsVariantsShoe.glb"},
     {name:"Duck", link:"https://modelviewer.dev/shared-assets/models/glTF-Sample-Models/2.0/Duck/glTF-Binary/Duck.glb"},
+    {name:"Robot", link:"https://modelviewer.dev/shared-assets/models/RobotExpressive.glb"},
+    {name:"SpinningRobot", link:"https://modelviewer.dev/shared-assets/models/RobotExpressive.glb"},
+    {name:"RunningRobot", link:"https://modelviewer.dev/shared-assets/models/RobotExpressive.glb"},
 ]
+
+let body = null
+let modelViewer = null;
+
+// let DEFAULT_NAME=""
+// let DEFAULT_SRC = "https://modelviewer.dev/shared-assets/models/NeilArmstrong.glb"
+// let DEFAULT_ALT_STR="Neil Armstrong's Spacesuit from the Smithsonian Digitization Programs Office and National Air and Space Museum"
 
 function openMenu(){
     document.getElementById("mySidenav").style.width = "250px";
@@ -14,6 +24,176 @@ function closeNav(){
 
 function changeProduct(name) {
     prod = products.find(item => item.name == name);
-    document.getElementById("model").src = prod.link;
+    modelViewer.src = prod.link;
+
+    if(prod.name=="SpinningRobot" || prod.name=="RunningRobot"){
+        modelViewer.setAttribute("autoplay", "");
+
+        if(prod.name=="RunningRobot"){
+            modelViewer.setAttribute("animation-name", "Running");
+        }
+    }else{
+        modelViewer.removeAttribute("animation-name");
+        modelViewer.removeAttribute("autoplay");
+    }
+
     closeNav();
-  }
+}
+
+// function createModelViewer(alt_str=DEFAULT_ALT_STR, src_path=DEFAULT_SRC){
+//     let model_viewer = document.createElement('model-viewer');
+// 	model_viewer.setAttribute("id", "model");
+// 	model_viewer.setAttribute("alt", alt_str);
+// 	model_viewer.setAttribute("src", src_path);
+// 	model_viewer.setAttribute("ar", "");
+// 	model_viewer.setAttribute("environment-image", "https://modelviewer.dev/shared-assets/environments/moon_1k.hdr");
+// 	model_viewer.setAttribute("poster", "https://modelviewer.dev/shared-assets/models/NeilArmstrong.webp");
+// 	model_viewer.setAttribute("shadow-intensity", "1");
+// 	model_viewer.setAttribute("camera-controls", "");
+// 	model_viewer.setAttribute("pan-y", "");
+
+//     let btn_model_viewer = document.createElement('button');
+//     btn_model_viewer.setAttribute("class", "ar-button");
+//     btn_model_viewer.setAttribute("slot", "ar-button");
+//     btn_model_viewer.setAttribute("pan-y", "");
+//     btn_model_viewer.textContent = "Activate AR";
+
+//     model_viewer.appendChild(btn_model_viewer)
+
+//     return model_viewer
+// }
+
+window.addEventListener("load", ()=> {
+    // body = document.getElementById("body");
+    // body.appendChild(model_viewer)
+
+    modelViewer = document.getElementById("model");
+
+    
+    // modelViewer.querySelector('#item-selector').addEventListener('onclick', (event) => {
+    //     modelViewer.src = event.target.value;
+    // });
+    
+    const checkbox = modelViewer.querySelector('#show-dimensions');
+    
+    function setVisibility(element) {
+        if (checkbox.checked) {
+        element.classList.remove('hide');
+        } else {
+        element.classList.add('hide');
+        }
+    }
+    
+    checkbox.addEventListener('change', () => {
+        setVisibility(modelViewer.querySelector('#dimLines'));
+        modelViewer.querySelectorAll('button').forEach((hotspot) => {
+        setVisibility(hotspot);
+        });
+    });
+    
+    // update svg
+    function drawLine(svgLine, dotHotspot1, dotHotspot2, dimensionHotspot) {
+        if (dotHotspot1 && dotHotspot2) {
+        svgLine.setAttribute('x1', dotHotspot1.canvasPosition.x);
+        svgLine.setAttribute('y1', dotHotspot1.canvasPosition.y);
+        svgLine.setAttribute('x2', dotHotspot2.canvasPosition.x);
+        svgLine.setAttribute('y2', dotHotspot2.canvasPosition.y);
+    
+        // use provided optional hotspot to tie visibility of this svg line to
+        if (dimensionHotspot && !dimensionHotspot.facingCamera) {
+            svgLine.classList.add('hide');
+        }
+        else {
+            svgLine.classList.remove('hide');
+        }
+        }
+    }
+    
+    const dimLines = modelViewer.querySelectorAll('line');
+    
+    const renderSVG = () => {
+        drawLine(dimLines[0], modelViewer.queryHotspot('hotspot-dot+X-Y+Z'), modelViewer.queryHotspot('hotspot-dot+X-Y-Z'), modelViewer.queryHotspot('hotspot-dim+X-Y'));
+        drawLine(dimLines[1], modelViewer.queryHotspot('hotspot-dot+X-Y-Z'), modelViewer.queryHotspot('hotspot-dot+X+Y-Z'), modelViewer.queryHotspot('hotspot-dim+X-Z'));
+        drawLine(dimLines[2], modelViewer.queryHotspot('hotspot-dot+X+Y-Z'), modelViewer.queryHotspot('hotspot-dot-X+Y-Z')); // always visible
+        drawLine(dimLines[3], modelViewer.queryHotspot('hotspot-dot-X+Y-Z'), modelViewer.queryHotspot('hotspot-dot-X-Y-Z'), modelViewer.queryHotspot('hotspot-dim-X-Z'));
+        drawLine(dimLines[4], modelViewer.queryHotspot('hotspot-dot-X-Y-Z'), modelViewer.queryHotspot('hotspot-dot-X-Y+Z'), modelViewer.queryHotspot('hotspot-dim-X-Y'));
+    };
+    
+    modelViewer.addEventListener('camera-change', renderSVG);
+    
+    modelViewer.addEventListener('load', () => {
+        const center = modelViewer.getBoundingBoxCenter();
+        const size = modelViewer.getDimensions();
+        const x2 = size.x / 2;
+        const y2 = size.y / 2;
+        const z2 = size.z / 2;
+    
+        modelViewer.updateHotspot({
+        name: 'hotspot-dot+X-Y+Z',
+        position: `${center.x + x2} ${center.y - y2} ${center.z + z2}`
+        });
+    
+        modelViewer.updateHotspot({
+        name: 'hotspot-dim+X-Y',
+        position: `${center.x + x2 * 1.2} ${center.y - y2 * 1.1} ${center.z}`
+        });
+        modelViewer.querySelector('button[slot="hotspot-dim+X-Y"]').textContent =
+            `${(size.z * 100).toFixed(0)} cm`;
+    
+        modelViewer.updateHotspot({
+        name: 'hotspot-dot+X-Y-Z',
+        position: `${center.x + x2} ${center.y - y2} ${center.z - z2}`
+        });
+    
+        modelViewer.updateHotspot({
+        name: 'hotspot-dim+X-Z',
+        position: `${center.x + x2 * 1.2} ${center.y} ${center.z - z2 * 1.2}`
+        });
+        modelViewer.querySelector('button[slot="hotspot-dim+X-Z"]').textContent =
+            `${(size.y * 100).toFixed(0)} cm`;
+    
+        modelViewer.updateHotspot({
+        name: 'hotspot-dot+X+Y-Z',
+        position: `${center.x + x2} ${center.y + y2} ${center.z - z2}`
+        });
+    
+        modelViewer.updateHotspot({
+        name: 'hotspot-dim+Y-Z',
+        position: `${center.x} ${center.y + y2 * 1.1} ${center.z - z2 * 1.1}`
+        });
+        modelViewer.querySelector('button[slot="hotspot-dim+Y-Z"]').textContent =
+            `${(size.x * 100).toFixed(0)} cm`;
+    
+        modelViewer.updateHotspot({
+        name: 'hotspot-dot-X+Y-Z',
+        position: `${center.x - x2} ${center.y + y2} ${center.z - z2}`
+        });
+    
+        modelViewer.updateHotspot({
+        name: 'hotspot-dim-X-Z',
+        position: `${center.x - x2 * 1.2} ${center.y} ${center.z - z2 * 1.2}`
+        });
+        modelViewer.querySelector('button[slot="hotspot-dim-X-Z"]').textContent =
+            `${(size.y * 100).toFixed(0)} cm`;
+    
+        modelViewer.updateHotspot({
+        name: 'hotspot-dot-X-Y-Z',
+        position: `${center.x - x2} ${center.y - y2} ${center.z - z2}`
+        });
+    
+        modelViewer.updateHotspot({
+        name: 'hotspot-dim-X-Y',
+        position: `${center.x - x2 * 1.2} ${center.y - y2 * 1.1} ${center.z}`
+        });
+        modelViewer.querySelector('button[slot="hotspot-dim-X-Y"]').textContent =
+            `${(size.z * 100).toFixed(0)} cm`;
+    
+        modelViewer.updateHotspot({
+        name: 'hotspot-dot-X-Y+Z',
+        position: `${center.x - x2} ${center.y - y2} ${center.z + z2}`
+        });
+    
+        renderSVG();
+    });
+  
+});
